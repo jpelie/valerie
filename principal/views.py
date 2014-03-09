806c1f533cabb5a62631f6d72c4bce3ea9980b55
Balde <louisa3160@gmail.com>
2014-03-09 18:32:37 -0400

    my django app

--- principal/views.py
+++ principal/views.py
@@ -8,13 +8,13 @@ def Mhome(request):
 
 
 def qui (request):
-    return render(request, '../OURPYTHONPRO/pages/Quisommes_nous.html', )
+    return render(request, '/pages/Quisommes_nous.html', )
 
 def balde(request):
-    return render(request,'../OURPYTHONPRO/pages/contact.html',)
+    return render(request,'/pages/contact.html',)
 
 def valerie (request):
-    return render(request,'../OURPYTHONPRO/pages/Accueil.html',)
+    return render(request,'/pages/Accueil.html',)
 
 def pedro (request):
-    return render(request,'../OURPYTHONPRO/pages/cours.html',)
+    return render(request,'/pages/cours.html',)
--- project/settings.py
+++ project/settings.py
@@ -26,6 +26,7 @@ TEMPLATE_DEBUG = True
 
 TEMPLATE_DIRS=(
     "project/Templates/",
+
 )
 ALLOWED_HOSTS = []
 
@@ -93,6 +94,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+)
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
