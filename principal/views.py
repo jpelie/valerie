a2a52722d16dcf3c413a70b1fde12234f198acb5
Balde <louisa3160@gmail.com>
2014-03-09 17:00:30 -0400

    my django app

--- principal/views.py
+++ principal/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def Mhome(request):
-    return render(request, 'OURPYTHONPRO\pages\index.html', )
+    return render(request, 'OURPYTHONPRO/pages/index.html', )
 
 
 def qui (request):
-    return render(request, 'OURPYTHONPRO\pages\Quisommes_nous.html', )
+    return render(request, 'OURPYTHONPRO/pages/Quisommes_nous.html', )
 
 def balde(request):
-    return render(request,'OURPYTHONPRO\pages\contact.html',)
+    return render(request,'OURPYTHONPRO/pages/contact.html',)
 
 def valerie (request):
-    return render(request,'OURPYTHONPRO\pages\Accueil.html',)
+    return render(request,'OURPYTHONPRO/pages/Accueil.html',)
 
 def pedro (request):
-    return render(request,'OURPYTHONPRO\pages\cours.html',)
+    return render(request,'OURPYTHONPRO/pages/cours.html',)
